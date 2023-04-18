82cb1a021c036146da1f40564e6b3885d600ea2e
ISYGBS07 <gomeisma@kkg-do.eu>
2023-04-18 10:41:36 +0200

    Initial commit

---
+++ src/MainController.java
@@ -0,0 +1,12 @@
+public class MainController {
+    public static void main(String[] args) {
+        for(int i = 0; i < 5; i++){
+            System.out.println("Wert von i: " + i);
+            if(i == 3){
+                System.out.println("Holdriho!");
+            }
+        }
+    }
+}
+
+
