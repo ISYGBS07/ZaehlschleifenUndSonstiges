19bb9843431a6fa1f837ad78ef6b6a37df912a38
ISYGBS07 <gomeisma@kkg-do.eu>
2023-04-25 11:20:42 +0200

    Ha Aufg.3 und 4 Komplett

--- src/MainController.java
+++ src/MainController.java
@@ -1,30 +1,58 @@
 public class MainController {
+    public static void main(String[] args) {
+        System.out.println(rectangleStars(10, 5));
 
-    private static int end;
-    private static int start;
-
-    public static boolean printSomeNumbers(){
-        for(int i = start; i <= end; i++){
-            System.out.println(i);
-        }
-        return false;
     }
 
+    private static void printSomeNumbers() {
+        for (int i = 6; i < 23; i++) {
+            System.out.println(i);
+        }
+    }
 
-    public static void main(String[] args) {
-        System.out.println(printSomeNumbers());
+    private static void printSomeNumbers(int start, int end) {
+        int sum = 0;
+        for (int i = start; i < end; i++) {
+            System.out.println(i);
+        }
     }
 
     public static int sumUp(int start, int end) {
-        int a = 0;
+        int sum = 0;
         int i;
         for (i = start; i <= end; i++) {
-            a += i;
+            sum = sum + i;
         }
-        return a;
+        return sum;
+    }
+
+    public static int powerOfTwo(int p) {
+        if (p < 0) return -1;
+
+        int result = 1;
+
+        for (int i = 0; i < p; i++) {
+            result = result * 2;
+        }
+        return result;
+    }
+
+    public static int sumUpPowerOfTwo(int n) {
+        int sum = 0;
+        for (int i = 0; i <= n; i++) {
+            sum = sum + powerOfTwo(i);
+        }
+        return sum;
+    }
+
+    public static int rectangleStars(int width, int height) {
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < width*height; i++) {
+                System.out.print("*");
+            }
+        }
+        return width;
     }
 }
-
-
 
 
